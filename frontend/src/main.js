74d7aec17738e8fd118ba227548901c8f9f2d77a
Jason <jason@deadtreepages.com>
2018-05-26 00:47:50 -0400

    Add google analytics.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,11 +6,16 @@ import Multiselect from 'vue-multiselect';
 import BarchefApp from './App';
 import router from './router';
 import '../node_modules/@fortawesome/fontawesome';
+import VueAnalytics from 'vue-analytics'
 
 Vue.config.productionTip = false;
 
 Vue.component('multiselect', Multiselect);
 Vue.use(VueScrollTo);
+Vue.use(VueAnalytics, {
+  id: 'UA-58723120-3',
+  router
+});
 
 /* eslint-disable no-new */
 new Vue({
